bede29e2ff8b1e37cfbaeedeb2f0629a3a96ba31
oskarm2003 <oskarm0503@gmail.com>
2025-02-12 12:41:05 +0100

    static mock images

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,7 @@ global.uploads_path = path.join(root_dir, "uploads")
 
 if (!process.env.DB_NAME)
     throw new Error("DB_NAME environmental variable not found")
+
 global.db_name = process.env.DB_NAME
 
 //init DB
